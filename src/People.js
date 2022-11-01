4881675d6e1eb08830a4aa42514be8f4a8d13a72
rmtkable <rmtkable@gmail.com>
2022-11-01 08:46:53 -0400

    first day of React Modules

---
+++ src/People.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class People extends Component{
+    render(){
+            return (
+                <div className="person">
+                    <div>Name: Ryan</div>
+                    <div>Age: 33</div>
+                    <div>Who Dey!</div>
+                </div>
+        )
+    }
+}
+
+export default People;
